7a0c48f721722743017a7f2c220ca21dc6c8053c
Kristina Sorochan <khristina.sorochan@gmail.com>
2020-11-01 17:10:04 +0200

    add routing

--- my-portfolio/src/app/app-routing.module.ts
+++ my-portfolio/src/app/app-routing.module.ts
@@ -1,7 +1,33 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {HomeComponent} from "./components/home/home.component";
+import {ServicesComponent} from "./components/services/services.component";
+import {SkillsComponent} from "./components/skills/skills.component";
+import {PortfolioComponent} from "./components/portfolio/portfolio.component";
+import {NotFoundComponent} from "./components/not-found/not-found.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: HomeComponent
+  },
+  {
+    path: 'services',
+    component: ServicesComponent
+  },
+  {
+    path: 'skills',
+    component: SkillsComponent
+  },
+  {
+    path: 'portfolio',
+    component: PortfolioComponent
+  },
+  {
+    path: '**',
+    component: NotFoundComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
