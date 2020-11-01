3afd2e8675c07e1fd355e9873ac10f8777a409e4
Kristina Sorochan <khristina.sorochan@gmail.com>
2020-11-01 16:18:31 +0200

    create components

--- my-portfolio/src/app/app.module.ts
+++ my-portfolio/src/app/app.module.ts
@@ -3,10 +3,20 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './components/home/home.component';
+import { NotFoundComponent } from './components/not-found/not-found.component';
+import { PortfolioComponent } from './components/portfolio/portfolio.component';
+import { ServicesComponent } from './components/services/services.component';
+import { SkillsComponent } from './components/skills/skills.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    NotFoundComponent,
+    PortfolioComponent,
+    ServicesComponent,
+    SkillsComponent
   ],
   imports: [
     BrowserModule,
