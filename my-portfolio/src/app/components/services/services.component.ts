eb3acd9cca54f3738dca293a10038bdcf85d58bc
Kristina Sorochan <khristina.sorochan@gmail.com>
2020-11-09 02:26:36 +0200

    update services page

--- my-portfolio/src/app/components/services/services.component.ts
+++ my-portfolio/src/app/components/services/services.component.ts
@@ -7,6 +7,29 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ServicesComponent implements OnInit {
 
+  services: any [] = [
+    {
+      iconUrl: 'assets/img/iconfinder_window-javascript-coding_532714.svg',
+      title: 'Web Development',
+      description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod'
+    },
+    {
+      iconUrl: 'assets/img/iconfinder_browser-web-content_532739.svg',
+      title: 'Web Design',
+      description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod'
+    },
+    {
+      iconUrl: 'assets/img/iconfinder_computer-devices-responsive_532706.svg',
+      title: 'Title',
+      description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod'
+    },
+    {
+      iconUrl: 'assets/img/iconfinder_clean-code-window_532737.svg',
+      title: 'Title',
+      description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod'
+    }
+  ]
+
   constructor() { }
 
   ngOnInit(): void {
