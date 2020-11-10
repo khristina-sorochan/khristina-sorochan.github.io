b0149b14cb40094b8c268cc41582457beda25c72
Kristina Sorochan <khristina.sorochan@gmail.com>
2020-11-10 03:01:29 +0200

    update skills page

--- my-portfolio/src/app/components/skills/skills.component.ts
+++ my-portfolio/src/app/components/skills/skills.component.ts
@@ -7,6 +7,49 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SkillsComponent implements OnInit {
 
+  skills: any [] = [
+    {
+      name: 'HTML5/CSS3',
+      value: '95'
+    },
+    {
+      name: 'Java Script',
+      value: '90'
+    },
+    {
+      name: 'Flexbox',
+      value: '95'
+    },
+    {
+      name: 'Bootstrap',
+      value: '75'
+    },
+    {
+      name: 'GitHub',
+      value: '60'
+    },
+    {
+      name: 'Angular',
+      value: '75'
+    },
+    {
+      name: 'Adobe Photoshop',
+      value: '95'
+    },
+    {
+      name: 'Adobe Lightroom',
+      value: '90'
+    },
+    {
+      name: 'Prototyping',
+      value: '95'
+    },
+    {
+      name: 'Figma',
+      value: '50'
+    }
+  ]
+
   constructor() { }
 
   ngOnInit(): void {
