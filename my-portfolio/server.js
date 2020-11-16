04bbb22fa1fa15d581549cf18489a58ccafdb144
Kristina Sorochan <khristina.sorochan@gmail.com>
2020-11-16 04:00:04 +0200

    setup deploy

---
+++ my-portfolio/server.js
@@ -0,0 +1,18 @@
+//Install express server
+const express = require('express');
+const path = require('path');
+
+const app = express();
+
+// Serve only the static files form the dist directory
+app.use(express.static(__dirname + '/dist/<my-portfolio>'));
+
+app.get('/*', function(req,res) {
+
+  res.sendFile(path.join(__dirname+'/dist/<my-portfolio>/index.html'));
+});
+
+// Start the app by listening on the default Heroku port
+app.listen(process.env.PORT || 8080, () => {
+  console.log(`Server start on port ${8080}`);
+});
