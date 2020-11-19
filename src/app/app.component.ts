c7ed84378e23361f84527186871101fe035c932c
Kristina Sorochan <khristina.sorochan@gmail.com>
2020-11-19 14:09:11 +0200

    clear code

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,6 +7,34 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
 
+  socials: any [] = [
+    {
+      name: 'Linkedin',
+      iconUrl: '/assets/img/LinkedIn.svg',
+      link: 'https://www.linkedin.com/in/kristina-sorochan-8842681b8/'
+    },
+    {
+      name: 'Github',
+      iconUrl: '/assets/img/Github.svg',
+      link: 'https://github.com/khristina-sorochan'
+    },
+    {
+      name: 'Behance',
+      iconUrl: '/assets/img/Behance.svg',
+      link: 'https://www.behance.net/khristinasorochan'
+    },
+    {
+      name: 'Facebook',
+      iconUrl: '/assets/img/Facebook.svg',
+      link: '#'
+    },
+    {
+      name: 'Instagram',
+      iconUrl: '/assets/img/Instagram.svg',
+      link: '#'
+    }
+  ];
+
   constructor() { }
 
   openNav(): void {
--- src/app/components/portfolio/portfolio.component.ts
+++ src/app/components/portfolio/portfolio.component.ts
@@ -7,6 +7,80 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PortfolioComponent implements OnInit {
 
+  filters: any [] = [
+    {
+      tag: 'all',
+      name: 'All'
+    },
+    {
+      tag: 'web-development',
+      name: 'Web Development'
+    },
+    {
+      tag: 'web-design',
+      name: 'Web Design'
+    },
+    {
+      tag: 'title-1',
+      name: 'Title'
+    },
+    {
+      tag: 'title-2',
+      name: 'Title'
+    }
+  ];
+
+  works: any [] = [
+    {
+      tag: 'web-development',
+      title: '',
+      imgUrl: 'assets/img/project-webdevelopment.jpg',
+      link: '#'
+    },
+    {
+      tag: 'web-design',
+      title: '',
+      imgUrl: 'assets/img/project-webdesign.jpg',
+      link: '#'
+    },
+    {
+      tag: 'web-development',
+      title: '',
+      imgUrl: 'assets/img/project-webdevelopment.jpg',
+      link: '#'
+    },
+    {
+      tag: 'title-1',
+      title: '',
+      imgUrl: 'assets/img/project-title1.jpg',
+      link: '#'
+    },
+    {
+      tag: 'title-2',
+      title: '',
+      imgUrl: 'assets/img/project-title2.jpg',
+      link: '#'
+    },
+    {
+      tag: 'title-1',
+      title: '',
+      imgUrl: 'assets/img/project-title1.jpg',
+      link: '#'
+    },
+    {
+      tag: 'web-design',
+      title: '',
+      imgUrl: 'assets/img/project-webdesign.jpg',
+      link: '#'
+    },
+    {
+      tag: 'web-development',
+      title: '',
+      imgUrl: 'assets/img/project-webdevelopment.jpg',
+      link: '#'
+    }
+  ];
+
   constructor() { }
 
   ngOnInit(): void {
